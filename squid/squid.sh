f073e33054bbb98d7c6671fd2499b891bf32e058
Wenhan Shi <wenhan.shi@rancher.com>
2021-04-21 17:05:04 +0900

    update script

--- squid/squid.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo apt update && sudo apt upgrade
-sudo apt -y install squid
-sudo systemctl start squid
-sudo sed -i -r 's/^http_access deny all/http_access allow all/' /etc/squid/squid.conf
-sudo systemctl reload squid
